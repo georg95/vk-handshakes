dd21cd8022cb9a67e0025f50efafe68c559861a8
Georg <georgsh-yateam@ya.ru>
2021-07-06 22:59:15 +0300

    fix error

--- index.js
+++ index.js
@@ -163,7 +163,9 @@ async function search() {
       var nextTierFriends = await getFriends(access_token, id)
       console.log('add', nextTierFriends.length, 'friends')
       addFriendsTier(friends, nextTierFriends, id)
-      document.getElementById(progressId).innerText = `${i}/${ids.length}`
+      if (document.getElementById(progressId)) {
+        document.getElementById(progressId).innerText = `${i}/${ids.length}`
+      }
       var commonFriends = getCommonFriends(friends1, friends2, id1, id2)
       if (commonFriends) {
         console.log('found:', commonFriends)
